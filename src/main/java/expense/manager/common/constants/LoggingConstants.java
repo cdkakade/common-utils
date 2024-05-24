4ac75cfc87eef67eb5a491f371450798d1116afd
cdkakade <cdkakade1@gmail.com>
2024-05-24 15:20:11 +0530

    Added constant

---
+++ src/main/java/expense/manager/common/constants/LoggingConstants.java
@@ -0,0 +1,10 @@
+package expense.manager.common.constants;
+
+public final class LoggingConstants {
+
+	private LoggingConstants() {
+	}
+
+	public final static String CORRELATION_ID = "expense-manager-correlation-id";
+
+}
