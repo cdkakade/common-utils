aacc33350a03741fd1c13b6347f42fda4657a7df
cdkakade <cdkakade1@gmail.com>
2024-05-24 15:20:11 +0530

    switched to Java 17 & Spring boot 3

--- src/main/java/expense/manager/common/dto/currency/request/CurrencyRequest.java
+++ src/main/java/expense/manager/common/dto/currency/request/CurrencyRequest.java
@@ -1,10 +1,9 @@
 package expense.manager.common.dto.currency.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
 
 @Data
 public class CurrencyRequest {
--- src/main/java/expense/manager/common/dto/wallet/request/WalletRequest.java
+++ src/main/java/expense/manager/common/dto/wallet/request/WalletRequest.java
@@ -1,11 +1,10 @@
 package expense.manager.common.dto.wallet.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
-
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
